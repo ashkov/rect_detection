d7fe0c990754b61e3a7270abb0d1aa5c34390a12
Александр Ашков <seberian@mail.ru>
2022-12-13 17:18:32 +0300

    fixed

--- rectangle.py
+++ rectangle.py
@@ -1,50 +1,50 @@
 import cv2
 import numpy as np
 
-base_colors = {
-    (255, 0, 0): "blue",
-    (127, 127, 127): "gray",
-    (0, 165, 255): "orange",
-    (0, 0, 0): "black",
-    (0, 255, 255): "yellow",
-    (255, 0, 255): "pink",
-    (0, 255, 0): "green",
-    (0, 0, 255): "red"
+hsv_colors = {
+    'gray': [[0, 0, 110], [0, 0, 150]],
+    'black': [[0, 0, 0], [0, 0, 10]],
+    'orange': [[18, 40, 90], [27, 255, 255]],
+    'red': [[0, 200, 200], [10, 255, 255]],
+    'pink': [[140, 200, 200], [170, 255, 255]],
+    'yellow': [[25, 50, 70], [35, 255, 255]],
+    'green': [[40, 50, 70], [70, 255, 255]],
+    'blue': [[110, 50, 70], [130, 255, 255]],
+
 }
 img = cv2.imread("img.jpg")
-threshold = 100
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-cv2.imshow("s", gray)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-
-ret, thresh = cv2.threshold(gray, 230, 255, cv2.THRESH_BINARY)
-cv2.imshow("s", thresh)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-image, contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 colors = {}
-for cnt in contours:
-    x1, y1 = cnt[0][0]
-    approx = cv2.approxPolyDP(cnt, 0.01 * cv2.arcLength(cnt, True), True)
-    if len(approx) > 0:
-        x, y, w, h = cv2.boundingRect(cnt)
-        color = (img[y + int(h / 2), x + int(w / 2)])
-        if np.array_equal([255, 255, 255], color):
-            continue
-        for bck in base_colors.keys():
-            if all(np.isclose(np.asarray(bck), color, atol=1.0)):
-                color_str = base_colors[bck]
-                break
-                # color_str = "_".join([str(c) for c in color])
-        if not color_str in colors:
-            colors[color_str] = 1
-        else:
-            colors[color_str] += 1
-        cv2.putText(img, color_str, (x1, y1), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 0), 2)
-        img = cv2.drawContours(img, [cnt], -1, (0, 255, 0), 3)
+for key, hc in hsv_colors.items():
+    hsv_min = np.array(hc[0], np.uint8)
+    hsv_max = np.array(hc[1], np.uint8)
+    filtered = cv2.inRange(hsv, hsv_min, hsv_max)
+    image, contours, hierarchy = cv2.findContours(filtered, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    for cnt in contours:
+        x1, y1 = cnt[0][0]
+        approx = cv2.approxPolyDP(cnt, 0.01 * cv2.arcLength(cnt, True), True)
+        if len(approx) > 0:
+            x, y, w, h = cv2.boundingRect(cnt)
+            color = (img[y + int(h / 2), x + int(w / 2)])
+            if np.array_equal([255, 255, 255], color):
+                continue
 
-cv2.imshow("Shapes", img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-print(colors)
+            if not key in colors:
+                colors[key] = 1
+            else:
+                colors[key] += 1
+            cv2.putText(img, key + str(len(approx)), (x1, y1), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 0), 2)
+            img = cv2.drawContours(img, [cnt], -1, (0, 255, 0), 3)
+color_max = None
+color_min = None
+count_max = None
+count_min = None
+
+for color in sorted(["gray", "black", "orange", "red", "pink", "yellow", "green", "blue"], reverse=True):
+    if count_max is None or colors[color] >= count_max:
+        count_max = colors[color]
+        color_max = color
+    if count_min is None or colors[color] <= count_min:
+        count_min = colors[color]
+        color_min = color
+print(colors, color_min, count_min, color_max, count_max)
